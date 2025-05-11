5a25010a7d900f4048536e6bd61c35384636942a
fredtso <fredriquetso@gmail.com>
2025-05-11 09:11:58 +0300

    working on javascript

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
+
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // web app Firebase configuration
@@ -14,11 +11,6 @@ const firebaseConfig = {
   appId: "1:1010812706852:web:90ce64afec09ee715d78b1",
   measurementId: "G-7450LRH3DT"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
